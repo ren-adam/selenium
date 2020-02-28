ae12157bb6a43096571ec3cee8ee9e8a1999019b
Peter <renpeter@126.com>
2020-02-28 17:17:25 +0800

    remove webdriver exe from project

--- release_browser/open_browser.py
+++ release_browser/open_browser.py
@@ -20,13 +20,13 @@ from selenium.webdriver.chrome.options import Options
 
 browser_stay_time = 5
 # normal mode
-chrome_driver = webdriver.Chrome(r'webdriver\chromedriver.80.exe')
+chrome_driver = webdriver.Chrome(r'D:\extpath\selenium\webdriver\chromedriver.80.exe')
 chrome_driver.get('http://www.cueb.edu.cn')
 time.sleep(browser_stay_time)
 chrome_driver.close()
 
 
-ie_driver = webdriver.Ie(r'webdriver\IEDriverServer.3.141.exe')
+ie_driver = webdriver.Ie(r'D:\extpath\selenium\webdriver\IEDriverServer.3.141.exe')
 ie_driver.get('http://www.cueb.edu.cn')
 time.sleep(browser_stay_time)
 ie_driver.close()
@@ -44,7 +44,7 @@ ie_driver.close()
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-chrome_driver = webdriver.Chrome(executable_path=r'webdriver\chromedriver.80.exe', options=chrome_options)
+chrome_driver = webdriver.Chrome(executable_path=r'D:\extpath\selenium\webdriver\chromedriver.80.exe', options=chrome_options)
 chrome_driver.get('http://www.cueb.edu.cn')
 time.sleep(browser_stay_time)
 print(chrome_driver.page_source)
